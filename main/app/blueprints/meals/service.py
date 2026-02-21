07d17d3fe2d21b952286e0eec92a1968846876ce
Sanjayc0204 <sanjaychunduru@gmail.com>
2026-02-21 12:52:46 -0500

    add update_meals method

--- main/app/blueprints/meals/service.py
+++ main/app/blueprints/meals/service.py
@@ -1,5 +1,6 @@
 from app.blueprints.meals.models import Meal
 from app.extensions import get_db
+from datetime import datetime
 
 
 class MealService:
@@ -13,3 +14,18 @@ class MealService:
             return [Meal(**meal) for meal in result]
         except Exception as e:
             raise Exception("Failed to get user meals")
+    
+    @staticmethod
+    def update_meals(uuid: str, date: datetime, meal: Meal) -> Meal:
+        db = get_db()
+
+        try:
+            meal_dict = meal.model_dump()
+            result = db.meals.update_one(
+                {"uuid": uuid, "date": date},
+                {"$set": meal_dict},
+                upsert=True
+            )
+            return meal
+        except Exception as e:
+            raise Exception("Failed to update meal")
