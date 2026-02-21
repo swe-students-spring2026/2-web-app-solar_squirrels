ef3bf2d522d18eff8f5e44f6727b803c1620f51a
Sanjayc0204 <sanjaychunduru@gmail.com>
2026-02-21 12:49:13 -0500

    add date parameter to get_user_meals

--- main/app/blueprints/meals/service.py
+++ main/app/blueprints/meals/service.py
@@ -5,11 +5,11 @@ from app.extensions import get_db
 class MealService:
     
     @staticmethod
-    def get_user_meals(uuid: str) -> list[Meal]:
+    def get_user_meals(uuid: str, date: datetime) -> list[Meal]:
         db = get_db()
 
         try:
-            result = db.meals.find({"user_id": uuid})
+            result = db.meals.find({"user_id": uuid, "date": date})
             return [Meal(**meal) for meal in result]
         except Exception as e:
             raise Exception("Failed to get user meals")
