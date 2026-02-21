124335a843276968bb4a55f0ed95601016c60c61
Sanjayc0204 <sanjaychunduru@gmail.com>
2026-02-21 12:44:11 -0500

    create routes.py for meals and create get_user_meals

---
+++ main/app/blueprints/meals/service.py
@@ -0,0 +1,15 @@
+from app.blueprints.meals.models import Meal
+from app.extensions import get_db
+
+
+class MealService:
+    
+    @staticmethod
+    def get_user_meals(uuid: str) -> list[Meal]:
+        db = get_db()
+
+        try:
+            result = db.meals.find({"user_id": uuid})
+            return [Meal(**meal) for meal in result]
+        except Exception as e:
+            raise Exception("Failed to get user meals")
